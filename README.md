b48db57c12bfd90e9448e61bed6555024d73df6f
enzoperce35 <enzoperce35@gmail.com>
2023-06-21 08:55:10 +0800

    modified object constructor from object prototyping to class syntax

--- README.md
+++ README.md
@@ -15,6 +15,7 @@
 ## Js lessons covered
   - object constructors
   - object prototyping
+  - object class syntax
 
 ## Note
   This is the first project from the 'TOP' Javascript course . There's so much more Js for me to learn. So, for code readabilty and orderliness, I used multiple Js file sheets for now.
--- helpers/book_object.js
+++ helpers/book_object.js
@@ -1,16 +1,19 @@
-function Book( title, author, pages, read ) {
-  this.title  = title;
-  this.author = author;
-  this.pages  = pages;
-  this.read   = read;
-}
+class Book {
 
-Book.prototype.addBookToLibrary = function() {
-  myLibrary.push(this)
-};
-
-Book.prototype.removeBookFromLibrary = function() {
-  if (window.confirm("Are you sure you want to remove this book?")) {
-    myLibrary.splice(myLibrary.indexOf(this), 1);
+  constructor( title, author, pages, read ) {
+    this.title  = title;
+    this.author = author;
+    this.pages  = pages;
+    this.read   = read;
   }
-};
+
+  addBookToLibrary() {
+    myLibrary.push(this)
+  }
+
+  removeBookFromLibrary() {
+    if (window.confirm("Are you sure you want to remove this book?")) {
+      myLibrary.splice(myLibrary.indexOf(this), 1);
+    }
+  }
+}
